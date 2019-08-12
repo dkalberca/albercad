9c30b63a6beba6eb33f9f5a9aa93a1d18a606858
dkalberca <53499684+dkalberca@users.noreply.github.com>
2019-08-12 18:31:11 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<h1>AlbercaDX Photo Gallery</h1>
 	<p><h2>List of Photos</h2></p>
 	<p><h3><a href="alberca1.php">Photo 1</a></h3></p>
-	<p><h3><a href="file:///C:/Users/DON/Desktop/School%20work/WEB/TestGallery3.html">Photo 2</a></h3></p>
-	<p><h3><a href="file:///C:/Users/DON/Desktop/School%20work/WEB/TestGallery4.html">Photo 3</a></h3></p>
+	<p><h3><a href="alberca2.php">Photo 2</a></h3></p>
+	<p><h3><a href="alberca3.php">Photo 3</a></h3></p>
 </body></center>
 </html>
