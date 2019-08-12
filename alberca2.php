3a0799b367e6c28a67e176bf8a319b4e08a99bde
dkalberca <53499684+dkalberca@users.noreply.github.com>
2019-08-12 18:33:39 +0800

    Update alberca2.php

--- alberca2.php
+++ alberca2.php
@@ -4,6 +4,6 @@
 <center><body>
 	<h1>AlbercaDX Photo Gallery</h1>
 	<p><img src="https://cnet2.cbsistatic.com/img/b94js3nbfScJiijk7o9jFmkk9PA=/1092x0/2019/01/29/ee91cb75-00ff-4b07-9e24-22100df8b801/pewds1.png" height="150" width="300"></p>
-	<p><a href="file:///C:/Users/DON/Desktop/School%20work/WEB/TestGallery.html">Back to Home</a>	||	<a href="file:///C:/Users/DON/Desktop/School%20work/WEB/TestGallery4.html">Next Photo</a></p>
+	<p><a href="index.php">Back to Home</a>	||	<a href="alberca3.php">Next Photo</a></p>
 </body></center>
 </html>
