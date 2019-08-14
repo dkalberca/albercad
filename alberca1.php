9bbb24d27862146221c56c49f240ab39635aea40
dkalberca <53499684+dkalberca@users.noreply.github.com>
2019-08-14 10:03:19 +0800

    Update alberca1.php

--- alberca1.php
+++ alberca1.php
@@ -4,6 +4,6 @@
 <center><body>
 	<h1>AlbercaDX Photo Gallery</h1>
 	<p><img src="https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1439,w_2560,x_0,y_0/dpr_1.5/c_limit,w_1044/fl_lossy,q_auto/v1492181001/articles/2015/05/06/morgan-freeman-shoots-straight-on-legalizing-marijuana-and-his-escape-from-new-york/150505-stern-morgan-freeman-tease_zomwym" height="150" width="300"></p>
-	<p><a href="index.php">Back to Home</a>	||	<a href="alberca2.php">Next Photo</a></p>
+	<p><a href="photogallery.php">Back to Home</a>	||	<a href="alberca2.php">Next Photo</a></p>
 </body></center>
 </html>
