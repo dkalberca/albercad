f0e8b71cc28fbf59003d2b40fa691dc93085a37e
dkalberca <53499684+dkalberca@users.noreply.github.com>
2019-08-14 10:07:33 +0800

    Update photogallery.php

--- photogallery.php
+++ photogallery.php
@@ -3,10 +3,10 @@
 <head><title>AlbercaDX's Gallery</title></head>
 <center><body>
 	<h1>AlbercaDX Photo Gallery</h1>
-	<p align="left"><a href="index.php>Logout</a></p>
 	<p><h2>List of Photos</h2></p>
 	<p><h3><a href="alberca1.php">Photo 1</a></h3></p>
 	<p><h3><a href="alberca2.php">Photo 2</a></h3></p>
 	<p><h3><a href="alberca3.php">Photo 3</a></h3></p>
+	<p><a href="index.php>Logout</a></p>
 </body></center>
 </html>
