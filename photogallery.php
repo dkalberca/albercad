500e480d5b5b7788b8f48c814db404dea766c05c
dkalberca <53499684+dkalberca@users.noreply.github.com>
2019-08-14 09:59:00 +0800

    Update and rename index.php to photogallery.php

--- photogallery.php
+++ photogallery.php
@@ -2,6 +2,7 @@
 <html>
 <head><title>AlbercaDX's Gallery</title></head>
 <center><body>
+	<p align="left"><a href="index.php>Logout</a></p>
 	<h1>AlbercaDX Photo Gallery</h1>
 	<p><h2>List of Photos</h2></p>
 	<p><h3><a href="alberca1.php">Photo 1</a></h3></p>
