19b472ae79ee1c698579ca7ee2d1f8acaa2e3000
dkalberca <53499684+dkalberca@users.noreply.github.com>
2019-08-14 10:04:00 +0800

    Update alberca3.php

--- alberca3.php
+++ alberca3.php
@@ -4,6 +4,6 @@
 <center><body>
 	<h1>AlbercaDX Photo Gallery</h1>
 	<p><img src="https://vignette.wikia.nocookie.net/mobile-legends/images/8/89/Fanny_Royal_Calvary_wall.jpg/revision/latest?cb=20181125173154&format=original" height="200" width="350"></p>
-	<p><a href="index.php">Back to Home</a>
+	<p><a href="photogallery.php">Back to Home</a>
 </body></center>
 </html>
